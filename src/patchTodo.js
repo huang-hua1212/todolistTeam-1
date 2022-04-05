1aa239be524f931805409940c4d31ba1d796c14a
Jo Yun <joyunhsiao@gmail.com>
2022-04-05 18:35:01 +0800

    fix syntax

--- src/patchTodo.js
+++ src/patchTodo.js
@@ -1,26 +1,19 @@
 function patchTodo(res, req, todos) {
-  req.on('end', () => {
-    try {
-      const todo = JSON.parse(body).title
-      const id = req.url.split('/').pop()
-      const index = todos.findIndex((element) => element.id == id)
-      if (todo !== undefined && index !== -1) {
-        todos[index].title = todo
-        res.writeHead(200, headers)
-        res.write(
-          JSON.stringify({
-            'status': 'success',
-            'data': todos,
-          }),
-        )
-        res.end()
-      } else {
-        errorHandle(res)
-      }
-    } catch {
-      errorHandle(res)
-    }
-  })
+  const todo = JSON.parse(req.body).title
+  const id = req.url.split('/').pop()
+  const index = todos.findIndex((element) => element.id == id)
+  if (todo !== undefined && index !== -1) {
+    todos[index].title = todo
+    res.status(200).json({
+      status: 'success',
+      data: todos,
+    })
+  } else {
+    res.status(400).json({
+      status: 'false',
+      message: 'title 欄位未填寫正確',
+    })
+  }
 }
 
 module.exports = patchTodo
